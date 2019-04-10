caa3231f1e6334d9072baa71a32c354af214c41f
Jaason <jaason@go2.pl>
2019-04-10 16:50:57 +0200

    chore(destroy): destroy

--- src/app/users/pages/user-page/user-page.component.ts
+++ src/app/users/pages/user-page/user-page.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../../service/users.service';
 import { User } from 'src/app/posts/interfaces/post.interface';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.scss']
 })
-export class UserPageComponent implements OnInit {
+export class UserPageComponent implements OnInit, OnDestroy{
 
   user: User = null;
   error = null;
+
+  destroy$ = new Subject();
 
   constructor(
     private route: ActivatedRoute,
@@ -23,10 +27,19 @@ export class UserPageComponent implements OnInit {
     this.setupUser();
   }
 
+  ngOnDestroy() {
+    this.destroy$.next(null);
+  }
+
   private setupUser() {
     const userId = this.route.snapshot.params.userId;
 
     this.usersService.getUserById(userId)
+    .pipe(
+      takeUntil(
+        this.destroy$
+      )
+    )
     .subscribe({
       next: (user) => {
         this.user = user;
