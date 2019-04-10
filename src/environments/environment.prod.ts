85731f7227327799ab9deeceb298c589b812f7c0
Jaason <jaason@go2.pl>
2019-04-10 10:18:53 +0200

    chore(env): prod env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  postsUrl: '/test-facebook/assets/posts.json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  postsUrl: './assets/posts.json'
+  postsUrl: '/assets/posts.json'
 
 };
 
